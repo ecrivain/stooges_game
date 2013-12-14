c5163f4cfa83f916853a56c6407b36736d1f51f5
ecrivain <jon@bowensnider.net>
2013-12-14 17:51:17 -0500

    first commit

--- untitled.rb
+++
@@ -1,9 +0,0 @@
-app = Proc.new do |env|
-  [200, { 'Content-Type' => 'text/plain' },
-    'Hello world!']
-end
-
-require 'rubygems'
-require 'thin'
-require 'rubyeventmachine'
-Rack::Handler::Thin.run(app, :Port => 4000)  
